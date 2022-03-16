be7957ff9a042b418a5b08f8f52a35deca3df957
Kaffeinated Kodemonkey <kaffeinatedkodemonkey@gmail.com>
2022-03-15 19:28:48 -0600

    Completed Part 6

---
+++ src/pages/blog/index.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <ul>
+              <li>
+                <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link> - Posted: {node.frontmatter.date}
+              </li>
+            </ul>            
+          </article>
+        ))
+      }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`
+
+export default BlogPage
